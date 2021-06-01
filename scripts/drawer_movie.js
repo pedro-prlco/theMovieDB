c4a17e30b8fea06bad98a8c12e3a574b0ff40d17
Pedro Pereira Lourenço <pedro.lourenco.1171956@sga.pucminas.br>
2021-06-01 12:20:41 -0300

    added movie tags

--- scripts/drawer_movie.js
+++ scripts/drawer_movie.js
@@ -1,5 +1,7 @@
 const posterOriginalBase = "https://image.tmdb.org/t/p/w500";
 
+const genreLi = "<li>GENRE_HERE</li>";
+
 $(document).ready(() => {
 
     var movie = JSON.parse(localStorage.getItem("inspectedMovie"));
@@ -8,4 +10,20 @@ $(document).ready(() => {
     $("#movie_info_poster").attr("src", posterOriginalBase.concat(movie.poster_path));
     $(".movie_info h2").text(movie.original_title);
     $(".movie_info p").text(movie.overview);
+
+    movie.production_companies.forEach(element => {
+        var old = $("ul").html();
+        $("ul").html(old + " " + genreLi.replace("GENRE_HERE", element.name));
+    });
+
+    movie.genres.forEach(element => {
+        var old = $("ul").html();
+        $("ul").html(old + " " + genreLi.replace("GENRE_HERE", element.name));
+    });
+
+    movie.spoken_languages.forEach(element => {
+        var old = $("ul").html();
+        $("ul").html(old + " " + genreLi.replace("GENRE_HERE", element.english_name));
+    });
+
 });
